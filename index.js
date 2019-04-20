f731631ceadaadd26aca2d2f8d5cb6d1289a00ed
care526 <710783534@qq.com>
2019-04-20 10:20:48 +0800

    1

--- index.js
+++ index.js
@@ -9,34 +9,27 @@ buttonNode.onclick = function () {
     alert ('密码错误');
   }
 }
-
+var imgs = text.length; // 88
+var times = Math.floor(imgs / 20);
+console.log(times)
 var main = document.getElementsByClassName('main')[0];
 main.style.visibility = 'hidden';
 var detail = document.getElementsByClassName('detail')[0];
 var back = document.getElementsByClassName('back')[0];
 var detail_imgs = document.getElementsByClassName('detail_imgs')[0];
+var pictures = document.getElementsByClassName('picture');
 var length = text.length;
 for (let i = length - 1; i > -1; i--) {
   let aNode = document.createElement('a');
   aNode.className = i + 1;
   let imgNode = document.createElement('img');
   imgNode.src = './jpg/' + (i + 1) + '-1';
-  // let txtNode = document.createElement('p');
-  // txtNode.innerHTML = text.detail[i];
   aNode.appendChild(imgNode);
-  // aNode.appendChild(txtNode);
   aNode.onclick = function () {
-    childNodes = detail_imgs.childNodes;
-    for (let i = childNodes.length - 1; i > -1; i--) {
-      detail_imgs.removeChild(childNodes[i])
-    }
-    detail.style.display = 'block';
     let ID = this.className;
-    for (let i = 1; i < 3; i++) {
-      let imgNode = document.createElement('img');
-      imgNode.src = './jpg/' + ID + '-' + i;
-      detail_imgs.appendChild(imgNode);
-    }
+    pictures[0].src = './jpg/' + ID + '-1';
+    pictures[1].src = './jpg/' + ID + '-2';
+    detail.style.display = 'block';
   }
   main.appendChild(aNode);
 }
